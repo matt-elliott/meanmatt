8d83eb010fd43074f3fd063553eb9ca388b6cc10
Matt Elliott <hello@mattelliott.us>
2018-01-03 17:07:13 -0800

    adding app.js

--- starter.sh
+++ starter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=/usr/local/bin:$PATH
-forever start -l /logs/forever.log -o /logs/output.log -e /logs/error.log
+forever start -l /logs/forever.log -o /logs/output.log -e /logs/error.log app.js
